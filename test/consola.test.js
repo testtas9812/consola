1700cce3c18f99120453b756aea54f89322d6eb8
testtas9812 <96862003+testtas9812@users.noreply.github.com>
2022-01-25 17:21:01 +0530

    Update consola.test.js

--- test/consola.test.js
+++ test/consola.test.js
@@ -52,7 +52,7 @@ describe('consola', () => {
     await wait(200)
     expect(logs.length).toBe(7)
     
-    // 6 + Last one indicating it repeated 4
+    // 6 + Last one indicating it repeated  4
     expect(logs[logs.length - 1].args).toEqual(['SPAM', '(repeated 4 times)'])
   })
 })
